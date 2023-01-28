f25013612477c66f01ebb37ba3009770e176574d
Wesley Siu <s10243612@connect.np.edu.sg>
2023-01-29 01:19:20 +0800

    Modified login page -Added toggle sign in and sign out function -Changed background of the page

--- login.js
+++ login.js
@@ -1,5 +1,24 @@
 // HAMBURGER MENU
 $(document).ready(()=>{
+    
+    const loginText = document.querySelector(".title-text .login");
+            const loginForm = document.querySelector("form.login");
+            const loginBtn = document.querySelector("label.login");
+            const signupBtn = document.querySelector("label.signup");
+            const signupLink = document.querySelector("form .signup-link a");
+            signupBtn.onclick = (()=>{
+              loginForm.style.marginLeft = "-50%";
+              loginText.style.marginLeft = "-50%";
+            });
+            loginBtn.onclick = (()=>{
+              loginForm.style.marginLeft = "0%";
+              loginText.style.marginLeft = "0%";
+            });
+            signupLink.onclick = (()=>{
+              signupBtn.click();
+              return false;
+            });
+
     $("#Submit").css({"pointer-events": "none"})
 
     //nav bar 
