8371c42da66a897e88cf4b9401c598ed1894639a
cipher <s10242598@connect.np.edu.sg>
2023-01-30 12:32:22 +0800

    added some scrolling animation to the home page -elements reveal on scroll

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 // HAMBURGER MENU
+$(document).ready(()=>{
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelector('.navbar-menu');
+    
+    menu.addEventListener('click', function(){
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    });
+    window.addEventListener("scroll",reveal);
+    function reveal(){
+        var reveals = document.querySelectorAll('.reveal')
+        var background = document.querySelectorAll('.background')
 
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar-menu');
+        for (var i=0; i<reveals.length; i++){
+            var windowheight = window.innerHeight;
+            var revealtop = reveals[i].getBoundingClientRect().top;
+            var revealpoint = 100;
+            if(revealtop< windowheight - revealpoint){
+                reveals[i].classList.add("active");
+                background[i].setAttribute("background","true")
 
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
+            }
+            else{
+                reveals[i].classList.remove('active');
+                background[i].setAttribute("background","false")
+
+
+            }
+
+        }
+    }
+})
