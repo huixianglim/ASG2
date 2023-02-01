8c9304c60e0cfba70fd067cdb609d63f0186c62e
Wesley Siu <s10243612@connect.np.edu.sg>
2023-02-01 16:19:59 +0800

    Added code for class js

--- class.js
+++ class.js
@@ -39,6 +39,34 @@ $(document).ready(()=>{
         console.log("hi")},200)
     }
     });
+
+    // const chars = ["./Images/knight.png", "./Images/darkninja.png", "./Images/tank.png"];
+    // const btns = ["k","n","t"];
+    // const smk = ["./Images/whitesmoke.png", "./Images/redsmoke.png", "./Images/yellowsmoke.png"]
+
+
+    // for(let i=0; i<chars.length; i++){
+    //     $(".character-button-" + btns[i] + " a").on("click", function(){
+    //     let char = chars[i];
+    //     let smoke = smk[i];
+                
+
+    //     $(".smoke-wrap").attr("smokeshow","false");
+    //     $(".character_spritesheet").css({"background":`url(${char}) no-repeat no-repeat`, "position":"absolute", "background-size":"100%", "width":"calc( var(--grid-cell)* 4 )", "height":"calc( var(--grid-cell)* 4 )", "image-rendering":"pixelated"});
+    //     $(".smoke,.smoke2,.smoke3").attr("src", `${smoke}`);
+    //     $(".smoke,.smoke2,.smoke3").css({"filter": "blur(5px)", "transform-origin":"50% 50%"});
+    //     $(".character").attr("animation","true");
+    //     if ($(".character").attr("animation") ==  "true"){
+    //         setTimeout(()=>{
+    //         $(".character").attr("animation","false");
+    //         $(".smoke-wrap").attr("smokeshow","true");
+    //         },200)
+    //     }
+    //     });
+
+
+    // }
+
     $(".select-btn").on("click",()=>{
         sessionStorage.setItem("character",character)
     })
